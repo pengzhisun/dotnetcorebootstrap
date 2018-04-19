0c45a821f2438f8c86145358a1f5e53ac2497601
Pengzhi Sun <pengzhisun@live.com>
2018-04-20 04:00:02 +0800

    move json and xml config file demo to dedicated cs file

---
+++ demos/config_demo/JsonFileConfigDemo.cs
@@ -0,0 +1,87 @@
+/******************************************************************************
+ * Copyright @ Pengzhi Sun 2018, all rights reserved.
+ * Licensed under the MIT License.
+ * See LICENSE file in the project root for full license information.  
+ *
+ * File Name:   JsonFileConfigDemo.cs
+ * Author:      Pengzhi Sun
+ * Description: .Net Core JSON format configuration file demos.
+ * Reference:   https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/ 
+ *              https://docs.microsoft.com/en-us/dotnet/api/microsoft.extensions.configuration
+ *              https://www.nuget.org/packages/Microsoft.Extensions.Configuration.Json
+ *              https://www.nuget.org/packages/Microsoft.Extensions.Configuration.Binder
+ *****************************************************************************/
+
+namespace DotNetCoreBootstrap.ConfigDemo
+{
+    using System;
+    using Microsoft.Extensions.Configuration;
+
+    /// <summary>
+    /// Defines the JSON format configuration file demo class.
+    /// </summary>
+    /// <remarks>
+    /// Depends on Nuget packages: Microsoft.Extensions.Configuration.Xml
+    /// and Microsoft.Extensions.Configuration.Binder .
+    /// </remarks>
+    internal static class JsonFileConfigDemo
+    {
+        /// <summary>
+        /// Run the demo.
+        /// </summary>
+        public static void Run()
+        {
+            IConfigurationBuilder configBuilder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", 
+                optional: true, 
+                reloadOnChange: true);
+            
+            IConfiguration config = configBuilder.Build();
+
+            Action<string, Func<object>> getValueAction =
+                (path, getValueFunc) =>
+                {
+                    object value = getValueFunc();
+                    Console.WriteLine($"path: '{path}', value: '{value}'");
+                };
+
+            // get string value demo
+            getValueAction(
+                "str_setting_1",
+                () =>
+                {
+                    string value = config["str_setting_1"];
+                    return value;
+                });
+
+            // get nested string value demo, using ':' delimiter
+            getValueAction(
+                "section1:nested_setting_1",
+                () =>
+                {
+                    string value = config["section1:nested_setting_1"];
+                    return value;
+                });
+
+            // get int value demo, using GetValue<T> method
+            getValueAction(
+                "int_setting_1",
+                () =>
+                {
+                    int value =
+                        config.GetValue<int>("int_setting_1", defaultValue: 0);
+                    return value;
+                });
+
+            // get array item value demo, using ':' delimiter and indexer 
+            getValueAction(
+                "array_section:0:item_setting",
+                () =>
+                {
+                    string value = config["array_section:0:item_setting"];
+                    return value;
+                });
+        }
+    }
+}
--- demos/config_demo/Program.cs
+++ demos/config_demo/Program.cs
@@ -1,22 +1,16 @@
 ﻿/******************************************************************************
  * Copyright @ Pengzhi Sun 2018, all rights reserved.
- * Licensed under the MIT License. See LICENSE file in the project root for full license information.  
+ * Licensed under the MIT License.
+ * See LICENSE file in the project root for full license information.  
  *
  * File Name:   Program.cs
  * Author:      Pengzhi Sun
- * Description: DotNetCore configuration demos.
- * Reference:   https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/ 
- *              https://docs.microsoft.com/en-us/dotnet/api/microsoft.extensions.configuration
- *              https://www.nuget.org/packages/Microsoft.Extensions.Configuration
- *              https://www.nuget.org/packages/Microsoft.Extensions.Configuration.Binder
- *              https://www.nuget.org/packages/Microsoft.Extensions.Configuration.Json
- *              https://www.nuget.org/packages/Microsoft.Extensions.Configuration.Xml
+ * Description: .Net Core configuration demos.
  *****************************************************************************/
 
-namespace Winl.DncBootstrap.ConfigDemo
+namespace DotNetCoreBootstrap.ConfigDemo
 {
     using System;
-    using Microsoft.Extensions.Configuration;
 
     /// <summary>
     /// Defines the demo console application.
@@ -29,8 +23,17 @@ namespace Winl.DncBootstrap.ConfigDemo
         /// <param name="args">The application command line arguments.</param>
         public static void Main(string[] args)
         {
-            RunDemo("JsonFileConfigDemo", JsonFileConfigDemo);
-            RunDemo("XmlFileConfigDemo", XmlFileConfigDemo);
+            Console.WriteLine(new string('#', 80));
+            Console.WriteLine($"# Begin .Net Core Configuartion Demos at {DateTime.Now}");
+            Console.WriteLine(new string('#', 80));
+            Console.WriteLine();
+
+            RunDemo("JsonFileConfigDemo", JsonFileConfigDemo.Run);
+            RunDemo("XmlFileConfigDemo", XmlFileConfigDemo.Run);
+
+            Console.WriteLine(new string('#', 80));
+            Console.WriteLine($"# End .Net Core Configuartion Demos at {DateTime.Now}");
+            Console.WriteLine(new string('#', 80));
         }
 
         /// <summary>
@@ -41,73 +44,13 @@ namespace Winl.DncBootstrap.ConfigDemo
         private static void RunDemo(string demoName, Action demoAction)
         {
             Console.WriteLine(new string('*', 80));
-            Console.WriteLine($"* run demo {demoName} at {DateTime.Now}");
+            Console.WriteLine($"* Run demo '{demoName}' at {DateTime.Now}");
             Console.WriteLine(new string('*', 80));
             Console.WriteLine();
 
             demoAction();
 
             Console.WriteLine();
-        }
-
-        /// <summary>
-        /// The JSON format configuration file demo.
-        /// </summary>
-        /// <remarks>
-        /// Depends on nuget packages: Microsoft.Extensions.Configuration.Json
-        /// and Microsoft.Extensions.Configuration.Binder .
-        /// </remarks>
-        private static void JsonFileConfigDemo()
-        {
-            IConfigurationBuilder configBuilder = new ConfigurationBuilder()
-                .SetBasePath(System.AppContext.BaseDirectory)
-                .AddJsonFile("appsettings.json", 
-                optional: true, 
-                reloadOnChange: true);
-            
-            IConfiguration config = configBuilder.Build();
-
-            string path = "section1:nested_setting_1";
-            Console.WriteLine($"path: '{path}', value: '{config[path]}'");
-
-            path = "str_setting_1";
-            Console.WriteLine($"path: '{path}', value: '{config[path]}'");
-
-            path = "int_setting_1";
-            Console.WriteLine($"path: '{path}', value: '{config.GetValue<int>(path, defaultValue: 0)}'");
-
-            path = "array_section:0:item_setting";
-            Console.WriteLine($"path: '{path}', value: '{config[path]}'");
-        }
-
-        /// <summary>
-        /// The XML format configuration file demo.
-        /// </summary>
-        /// <remarks>
-        /// Depends on nuget packages: Microsoft.Extensions.Configuration.Xml
-        /// and Microsoft.Extensions.Configuration.Binder .
-        /// </remarks>
-        private static void XmlFileConfigDemo()
-        {
-            IConfigurationBuilder configBuilder = new ConfigurationBuilder()
-                .SetBasePath(System.AppContext.BaseDirectory)
-                .AddXmlFile("appsettings.xml", 
-                optional: true, 
-                reloadOnChange: true);
-                
-            IConfiguration config = configBuilder.Build();
-
-            string path = "section1:nested_setting_1";
-            Console.WriteLine($"path: '{path}', value: '{config[path]}'");
-
-            path = "str_setting_1";
-            Console.WriteLine($"path: '{path}', value: '{config[path]}'");
-
-            path = "int_setting_1";
-            Console.WriteLine($"path: '{path}', value: '{config.GetValue<int>(path, defaultValue: 0)}'");
-
-            path = "array_items:array_item:item_1:item_setting";
-            Console.WriteLine($"path: '{path}', value: '{config[path]}'");
         }
     }
 }
